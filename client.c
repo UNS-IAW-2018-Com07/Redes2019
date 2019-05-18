8f8235b6c59e332ba8fbd4fc7fa1d08842597abf
StephanieMalvicini <s.a.m_puan@hotmail.com>
2019-05-18 14:13:41 -0300

    se obtiene el dns por defecto del resolv.conf

--- client.c
+++ client.c
@@ -8,21 +8,48 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include<unistd.h>
+
+/*
+ * Get the DNS servers from /etc/resolv.conf file on Linux
+ * */
+in_addr_t get_dns_server()
+{
+    FILE *nameservers_file;
+    char readed_line[200], *dns_address;
+
+    if((nameservers_file = fopen("/etc/resolv.conf", "r")) == NULL)
+    {
+        printf("ACA DEBERIAMOS DECIR QUE NO SE PUDO REALIZAR LA CONSULTA PRQUE NO HAY DNS EN LOS PARAMS NI SABEMOS EL DEFAULT  \n");
+    }
+     
+    while(fgets(readed_line, 200 ,nameservers_file))
+    {
+        if(readed_line[0] != '#')
+        {
+            if(strncmp(readed_line, "nameserver", 10) == 0)
+            {
+                dns_address = strtok(readed_line, " ");
+                dns_address = strtok(NULL, " ");
+                return inet_addr(dns_address);
+            }
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     extern int errno; 
-    //extern char *sys_errlist[]; 
     
-    const int portnum = 126; 
+    const int portnum = 53; 
     int socket_file_descriptor;
     struct sockaddr_in server;
     struct hostent *server_host; 
-    char buffer[1024];
+    char buffer[512];
 
-    bzero(&server, sizeof(server)); 
+    bzero(&server, sizeof(server));
     server.sin_family = AF_INET; 
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = get_dns_server();
     server.sin_port = htons((short) portnum); 
     
     // Creacion del socket - devuelve -1 si da error 
