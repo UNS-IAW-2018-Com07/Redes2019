87067a715070f8ac1e53ba466b1bc5c1029febc1
Caterina-Panzone <caterina.p@hotmail.com>
2019-05-19 14:05:22 -0300

    funciona el envio de la consulta

--- client.c
+++ client.c
@@ -16,7 +16,7 @@ void ChangetoDnsNameFormat(unsigned char* dns,unsigned char* host);
 
 int main( int argc , char *argv[])
 {
-    unsigned char* hostname = (unsigned char*) "3www6google3com";
+    unsigned char* hostname= "www.google.com";
     unsigned short qtype = 1;
     extern int errno; 
     
@@ -34,7 +34,6 @@ int main( int argc , char *argv[])
 
     struct DNS_HEADER *query_header = (struct DNS_HEADER *) &query;
     query_header->id = (unsigned short) htons(getpid());
-    printf("id:: %d \n",ntohs(query_header->id));
     query_header->qr = 0; 
     query_header->opcode = 0; 
     query_header->aa = 0; 
@@ -48,11 +47,21 @@ int main( int argc , char *argv[])
     query_header->ns_count = 0;
     query_header->ar_count = 0;
 
-    struct QUESTION *query_question = (struct QUESTION *) &query[sizeof(struct DNS_HEADER)];
-    query_question->qname = hostname;
-    query_question->qtype = htons(qtype);
-    query_question->qclass = htons(1); // Poner una constante para internet
-    
+    unsigned char *qname = (unsigned char *) &query[sizeof(struct DNS_HEADER)];
+    ChangetoDnsNameFormat(qname, hostname);
+
+    struct QUESTION_CONSTANT *q_constant = (struct QUESTION_CONSTANT *) &query[sizeof(struct DNS_HEADER)+ (strlen((const char*)qname) + 1)];
+    q_constant->qtype = htons(qtype);
+    q_constant->qclass = htons(1); // Poner una constante para internet
+    //struct QUESTION *query_question = (struct QUESTION *) &query[sizeof(struct DNS_HEADER)];
+    //unsigned char *qname = malloc(sizeof(hostname)+2);
+    //qname = query_question->qname;
+    //ChangetoDnsNameFormat(qname, hostname);
+    //query_question->qname = hostname;
+    //&query_question->qtype = 
+    //query_question->qtype = htons(qtype);
+    //query_question->qclass = htons(1); // Poner una constante para internet
+
     // Creacion del socket - devuelve -1 si da error 
     if((socket_file_descriptor = socket(AF_INET, SOCK_DGRAM, 0))<0)
     {
@@ -60,7 +69,25 @@ int main( int argc , char *argv[])
         exit(errno); 
     }
 
-    if((sendto(socket_file_descriptor, query, sizeof(struct DNS_HEADER) + sizeof(struct QUESTION), 0, (struct sockaddr*)&server, sizeof(server)))<0)
+    printf("Id:: %d \n",ntohs(query_header->id));
+    printf("Puntero Id:: %p \n",&query_header->id);
+    
+    printf("Rd:: %d \n",ntohs(query_header->rd));
+    printf("TC:: %d \n",ntohs(query_header->tc));
+    printf("AA:: %d \n",ntohs(query_header->aa));
+    printf("opcode:: %d \n",ntohs(query_header->opcode));
+    printf("QR:: %d \n",ntohs(query_header->qr));
+
+    printf("Rcode:: %d \n",ntohs(query_header->rcode));
+    printf("Z:: %d \n",ntohs(query_header->z));
+    printf("RA:: %d \n",ntohs(query_header->ra));
+
+    printf("QdCount:: %d \n",ntohs(query_header->qd_count));
+    printf("AnswerCount:: %d \n",ntohs(query_header->an_count));
+    printf("NsCount:: %d \n",ntohs(query_header->ns_count));
+    printf("ArCount:: %d \n",ntohs(query_header->ar_count));
+
+    if((sendto(socket_file_descriptor, query, sizeof(struct DNS_HEADER) +  (strlen((const char*)qname)+1) + sizeof(struct QUESTION_CONSTANT), 0, (struct sockaddr*)&server, sizeof(server)))<0)
     {
         perror("Error al intentar enviar un mensaje al servidor. \n");
         exit(errno); 
@@ -78,15 +105,29 @@ int main( int argc , char *argv[])
         exit(errno); 
     }
     printf("[+]Data Received: %li \n", strlen(response));
-    printf("[+]Data Received: %i \n", ntohs(response[0]));
 
     printf("Id:: %d \n",ntohs(query_response->id));
-    printf("QR:: %d \n",ntohs(query_response->qr));
-    printf("Code:: %d \n",ntohs(query_response->rcode));
-    printf("Answer:: %d \n",ntohs(query_response->an_count));
-    printf("Qd:: %d \n",ntohs(query_response->qd_count));
-    printf("Ns:: %d \n",ntohs(query_response->ns_count));
+    printf("Puntero Id:: %p \n",&query_response->id);
 
+    printf("Rd:: %d \n",ntohs(query_response->rd));
+    printf("TC:: %d \n",ntohs(query_response->tc));
+    printf("AA:: %d \n",ntohs(query_response->aa));
+    printf("opcode:: %d \n",ntohs(query_response->opcode));
+    printf("QR - Ntohs:: %d \n",ntohs(query_response->qr));
+    printf("QR - Htons:: %d \n",htons(query_response->qr));
+    printf("QR - Sin Nada:: %d \n",query_response->qr);
+
+    printf("Rcode - Htons:: %d \n",ntohs(query_response->rcode));
+    printf("Rcode - Htons:: %d \n",htons(query_response->rcode));
+    printf("Rcode - Sin nada:: %d \n",query_response->rcode);
+    printf("Z:: %d \n",ntohs(query_response->z));
+    printf("RA:: %d \n",ntohs(query_response->ra));
+
+    printf("QdCount:: %d \n",ntohs(query_response->qd_count));
+    printf("AnswerCount:: %d \n",ntohs(query_response->an_count));
+    printf("NsCount:: %d \n",ntohs(query_response->ns_count));
+    printf("ArCount:: %d \n",ntohs(query_response->ar_count));
+    
     return 0;
 }
 
@@ -123,20 +164,24 @@ in_addr_t get_query_server()
  * */
 void ChangetoDnsNameFormat(unsigned char* dns,unsigned char* host) 
 {
+    unsigned char *host_copy = malloc(sizeof(strlen(host) + 1));
+    strcpy(host_copy,host);
+
     int lock = 0 , i;
-    strcat((char*)host,".");
+    strcat((char *) host_copy,".");
      
-    for(i = 0 ; i < strlen((char*)host) ; i++) 
+    for(i = 0 ; i < strlen((char*)host_copy) ; i++) 
     {
-        if(host[i]=='.') 
+        if(host_copy[i]=='.') 
         {
             *dns++ = i-lock;
             for(;lock<i;lock++) 
             {
-                *dns++=host[lock];
+                *dns++=host_copy[lock];
             }
             lock++; //or lock=i+1;
         }
     }
     *dns++='\0';
+    free(host_copy);
 }
--- message_elements.c
+++ message_elements.c
@@ -2,36 +2,41 @@
 
 struct DNS_HEADER
 {
-    
-    unsigned char qr :1;     // identifica si es una consulta (0) o una respuesta (1)
-    unsigned char opcode :4; // especifica el tipo de consulta: estandar (0), inversa (1), estado servidor (2)
-    unsigned char aa :1;     // se usa en las respuestas y especifica que el dns que otorga la respuesta es autoritativo para el dominio consultado 
-    unsigned char tc :1;     // especifica si el mensaje fue truncado por tener un tamanio mayor al permitido
+    unsigned short id;       // se usa para unir las respuestas con las solicitudes  
+
     unsigned char rd :1;     // se setea en la consulta y se copia en la respuesta. Consulta resuelta: iterativamente (0) y recursivamente (1)
-    unsigned char ra :1;     // especifica si el servidor puede responder recursivamente
-    unsigned char z :1;      // tiene que valer 0. Reservado para uso futuro    
-    unsigned char rcode :4;  // Cdigo de respuesta: 
+    unsigned char tc :1;     // especifica si el mensaje fue truncado por tener un tamanio mayor al permitido
+    unsigned char aa :1;     // se usa en las respuestas y especifica que el dns que otorga la respuesta es autoritativo para el dominio consultado 
+    unsigned char opcode :4; // especifica el tipo de consulta: estandar (0), inversa (1), estado servidor (2)
+    unsigned char qr :1;     // identifica si es una consulta (0) o una respuesta (1)
+
+    unsigned char rcode :4;  // Codigo de respuesta: 
                                 // (0) No hubo error. 
                                 // (1) El servidor no puedo interpretar la consulta 
                                 // (2) El DNS tuvo un problema y no puede procesar la consulta 
                                 // (3) El dominio referenciado no existe. Solamente tiene significado cuando responde un DNS autoritativo 
                                 // (4) El dns no soporta la consulta requerida
                                 // (5) El dns se rehusa a realizar la operacion especificada por razones de politicas
+    unsigned char z :3;      // tiene que valer 0. Reservado para uso futuro
+    unsigned char ra :1;     // especifica si el servidor puede responder recursivamente
+    
     unsigned short qd_count; // numero de entradas de la seccion QUESTION
     unsigned short an_count; // numero de entradas de la seccion ANSWER
     unsigned short ns_count; // numero de resource records de DNS que estan dentro de la seccion AUTHORITY 
-    unsigned short ar_count; // numero de resource records en la seccion ADDITIONAL
-    //unsigned char cd :1;     // checking disabled
-    //unsigned char ad :1;     // authenticated data  
-    unsigned short id;       // se usa para unir las respuestas con las solicitudes  
+    unsigned short ar_count; // numero de resource records en la seccion ADDITIONAL    
 };
 
-struct QUESTION
+struct QUESTION_CONSTANT
 {
-    unsigned char *qname;   // nombre simbolico que se quiere resolver
     unsigned short qtype;   // tipo del campo del dns que se desea consultar
     unsigned short qclass;  // clase de la consulta: IN (internet)
 };
+
+typedef struct
+{
+    unsigned char *qname;   // nombre simbolico que se quiere resolver
+    struct QUESTION_CONSTANT *ques;
+} QUESTION;
 
 struct RES_RECORD
 {
