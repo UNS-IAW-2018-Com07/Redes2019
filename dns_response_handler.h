56c2395e06440a3c735b0a445afd1e78987c6dcb
StephanieMalvicini <s.a.m_puan@hotmail.com>
2019-06-01 16:17:58 -0300

    arregle el mx

--- dns_response_handler.h
+++ dns_response_handler.h
@@ -1,7 +1,7 @@
 #include <arpa/inet.h>
 #include "dns_response_printer.h"
 
-int handleResponse(unsigned char *response, int qname_length, int show_if_no_answer);
+void handleResponse(unsigned char *response, int qname_length);
 
 in_addr_t* getNextServer(unsigned char *response, unsigned char* hostname, int qname_length, unsigned char **dom_name);
 
